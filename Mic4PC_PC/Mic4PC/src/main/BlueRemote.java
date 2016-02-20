455088c6933a28d40ec55eca1d0249749c7b8e7c
dong yuangui <dongyuangui@appadhoc.com>
2016-02-20 09:45:00 +0800

    init

---
+++ Mic4PC_PC/Mic4PC/src/main/BlueRemote.java
@@ -0,0 +1,25 @@
+package main;
+
+import javax.swing.JOptionPane;
+
+import bluetooth.BlueService;
+import components.SysTray;
+
+/**
+ * @author royen
+ * @since 2010.2.10
+ */
+public class BlueRemote {
+	
+	public static void main(String[] args){
+		BlueService bs=new BlueService();
+		
+		if(bs.initialBluetooth()){
+			new SysTray().show();
+			bs.startService();
+		}
+		else{
+			JOptionPane.showMessageDialog(null,"-------------------------------------------");
+		}
+	}
+}
